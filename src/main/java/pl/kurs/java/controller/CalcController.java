154e5f65a4b0024728c5fced4978762c09776fec
niczegose <black.holes.22@gmail.com>
2020-08-11 19:47:28 +0200

    calculator updates

--- src/main/java/pl/kurs/java/controller/CalcController.java
+++ src/main/java/pl/kurs/java/controller/CalcController.java
@@ -17,16 +17,25 @@ public class CalcController {
 
     @GetMapping
     public String processCalculation(@RequestParam(value = "number1", required = false) String number1,
-                                     @RequestParam(value ="number2", required = false) String number2,
-                                     @RequestParam(value ="operator", required = false) String operator,
+                                     @RequestParam(value = "number2", required = false) String number2,
+                                     @RequestParam(value = "operator", required = false) String operator,
                                      ModelMap model) {
 
         // for testing purpose:
         System.out.println("Values to calculate: " + number1 + " and " + number2 + " and " + operator);
 
-        if (number1!=null && number2!=null && operator!=null){
-            String message = number1 + " " + operator + " " + number2;
-            String result="";
+        if (number1 != null && number2 != null && operator != null) {
+            String message;
+            if (number1.equals("")) {
+                message = number2.trim();
+            } else if (number2.equals("")) {
+                message = number1.trim();
+            } else {
+                message = number1.trim() + " " + operator + " " + number2.trim();
+            }
+
+            String result = "";
+
             try {
                 Equation.isNawias(message);
                 result = Equation.obliczWyrazenie(message);
--- src/test/java/pl/kurs/java/controller/CalcControllerTest.java
+++ src/test/java/pl/kurs/java/controller/CalcControllerTest.java
@@ -1,7 +1,140 @@
 package pl.kurs.java.controller;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CalcController.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class CalcControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void shouldAddNumbers() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                    .param("number1", "1")
+                    .param("number2", "2")
+                    .param("operator", "+"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("1 + 2 = 3")));
+    }
+
+    @Test
+    public void shouldSubtractNumbers() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "2")
+                        .param("number2", "2")
+                        .param("operator", "-"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("2 - 2 = 0")));
+    }
+
+    @Test
+    public void shouldMultiplyNumbers() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "2")
+                        .param("number2", "2")
+                        .param("operator", "*"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("2 * 2 = 4")));
+    }
+
+    @Test
+    public void shouldDivideNumbers() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "2")
+                        .param("number2", "2")
+                        .param("operator", "/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("2 / 2 = 1")));
+    }
+
+    @Test
+    public void shouldNotDivideNumbers() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "2")
+                        .param("number2", "0")
+                        .param("operator", "/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("incorrect input")));
+    }
+
+    @Test
+    public void shouldAddNumbersEnteredIntoOneParameterWithAdditionalSpace() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "1 + 2")
+                        .param("number2", "")
+                        .param("operator", ""))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("1 + 2 = 3")));
+    }
+
+    @Test
+    public void shouldAddMultiplyNumbers() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "1 + 2")
+                        .param("number2", "2")
+                        .param("operator", "+"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("1 + 2 + 2 = 5")));
+    }
+
+    @Test
+    public void shouldAddMultiplyNumbersFromOneParamWithAdditionalOperator() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "1 + 2")
+                        .param("number2", "")
+                        .param("operator", "+"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("1 + 2 = 3")));
+    }
+
+    @Test
+    public void shouldAddMultiplyNumbersFromOneParamWithAdditionalWrongOperator() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "")
+                        .param("number2", "1 + 2")
+                        .param("operator", "*"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("1 + 2 = 3")));
+    }
+
+    @Test
+    public void shouldAddNumbersWithAdditionalSpace() throws Exception {
+        mockMvc.perform(
+                get("/calc")
+                        .param("number1", "1 ")
+                        .param("number2", "2")
+                        .param("operator", "+"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(request().attribute("message", containsString("1 + 2 = 3")));
+    }
 }
